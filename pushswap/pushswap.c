ee20547fa6c2b48e17bfa398d74b62468cf44ad0
Finola CAHILL <fcahill@e3r10p12.42.fr>
2019-06-02 23:43:10 +0200

    integrating b into sorting 2/3/5

--- pushswap/pushswap.c
+++ pushswap/pushswap.c
@@ -20,10 +20,16 @@ int			main(int argc, char **argv)
 			return (ft_pushswap_error());
 	//	ft_print_stacks(&stack); // mini visu to be deleted later
      //   ft_sort_a(&stack, 0, stack.a_count);
-	// ft_sort_two(&stack);
-//	 ft_sort_three(&stack, 'a');
-	ft_sort_five(&stack, 'a');
-	printf(" median = %d\n", find_median(stack.a, 0, stack.a_count));
+	
+//	 
+	int i = 0;
+	while(i++ < 3)
+		ft_move("pb", &stack);
+		ft_sort_three(&stack, 'b');
+	//ft_sort_two(&stack);
+	//ft_sort_five(&stack, 'b');
+	
+	//printf(" median = %d\n", find_median(stack.a, 0, stack.a_count));
 	ft_print_stacks(&stack); // mini visu to be deleted later
 	}
 	return (0);
--- sort_5.c
+++ sort_5.c
@@ -30,16 +30,29 @@ void     ft_sort_three(t_stacks *stack, char name)
 {
     ft_sort_two(stack);
 
-    if (name == 'a' && stack->a_count == 3)
-    {
+  //  if (stack->a_count == 3)
+   // {
         ft_sort_two(stack);
-        if (stack->a[2] < stack->a[1] || stack->a[2] < stack->a[0])
+        if (name == 'a') 
+        {
+            if (stack->a[2] < stack->a[1] || stack->a[2] < stack->a[0])
             {
-                ft_move ("rra", stack);
+                ft_move("rra", stack);
                 ft_sort_two(stack);
             }
+        }
+        if (name == 'b')
+        {
+            if (stack->b[2] > stack->b[1] || stack->b[2] > stack->b[0])
+            {
+                ft_move("rrb", stack);
+                ft_sort_two(stack);
+            }
+        }
+        
 
-    }
+ //   }
+    
 
 }
 
@@ -51,7 +64,8 @@ void    ft_sort_five(t_stacks *stack, char name)
 
     rev = 0;
     push = 0;
-    if (name == 'a' && stack->a_count == 5) 
+
+    if (name == 'a') 
     {
         pivot = find_median(stack->a, 0, stack->a_count);
         while (rev + push < 5)
@@ -60,21 +74,34 @@ void    ft_sort_five(t_stacks *stack, char name)
             {
                 ft_move("pb", stack);
                 ++push;
-                ft_print_stacks(stack);
             }
             if (stack->a[0] >= pivot)
             {
              ft_move("ra", stack);
              ++rev;
-             ft_print_stacks(stack);
             }
         }
+    }
+    if (name == 'b')
+    {
+        pivot = find_median(stack->b, 0, stack->b_count);
+     
         if (push == 2)
         {
             ft_sort_two(stack);
-            ft_sort_three(stack, 'a');
-            ft_move("pa", stack);
-            ft_move("pa", stack);
+            ft_sort_three(stack, name);
+            if (name == 'a')
+            {
+                ft_move("pa", stack);
+                ft_move("pa", stack);
+            }   
+            if (name == 'b')
+            {
+                ft_move("pb", stack);
+                ft_move("pb", stack);
+            } 
         }
    }
 }
+
+//void    ft_sort_ten(t_stacks &stack);
